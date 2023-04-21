f5cf96f82f8b19aa4f4a7f8a93886705ed076b72
Juliana Guimarães <jujucg15@gmail.com>
2023-04-21 18:51:21 -0300

    ✨ Criação do card de colaborador

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,3 +1,4 @@
+import { Colaborador } from '../Colaborador'
 import './Time.css'
 
 export const Time = (props) => {
@@ -6,6 +7,7 @@ export const Time = (props) => {
     return (
         <section className = "time" style={css}>
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
+            <Colaborador/>
         </section>
     )
 }
